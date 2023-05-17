ed9b734c9172acb10e1aaa9b331caae4b4d4738c
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-05-17 19:45:13 +0530

    sum to two numbers

---
+++ pr1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,sum=0;
+    printf("hey user enter the first num-");
+    scanf("%d",&a);
+    printf("hey user enter the second num-");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum= %d",sum);
+
+}
