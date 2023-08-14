fd767cc5c4e409f0f03cd566bd588391fb76834f
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-08-14 12:03:12 +0530

    checking true or false

---
+++ plus.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int num1=9, num2=9;
+    float pie=3.14;
+    
+    if(num1!=num2)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+}
