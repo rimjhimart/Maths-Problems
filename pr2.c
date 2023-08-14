4afa9357b6c468c2d5113a8c1adb1ef6eae07821
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-08-14 11:45:29 +0530

    biggest number between two

---
+++ pr2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    int a,b;
+    printf("1st- ");
+    scanf("%d",&a);
+    printf("2nd- ");
+    scanf("%d",&b);
+    if (a>b)
+    {
+        printf("biggest num %d",a);
+    }
+    else
+    {
+        printf("biggest num %d",b);
+    }
+
+}
