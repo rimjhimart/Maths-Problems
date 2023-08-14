011c1995a76de9d21a22d8ed056b016b0a717369
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-08-14 11:46:37 +0530

    factorial of two numbers

---
+++ pr4.c
@@ -0,0 +1,14 @@
+//factorial of a number 
+#include<stdio.h>
+void main()
+{
+    int k,i=1,kfact=1;
+    printf("enter the number");
+    scanf("%d",&k);
+    
+    for(int i=1;i>=kfact;i++)
+    {
+       fact*i=i;
+    }
+    printf("factroial of the num %d = %d",k,kfact);
+}
