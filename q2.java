d8ef2cc7904608a2a2f4db2dfe4787e39c40a35a
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-08-19 17:24:08 +0530

    sum of three numbers in java

---
+++ q2.java
@@ -0,0 +1,14 @@
+//SumOfNumbers3
+
+public class SumOfNumbers3  
+{    
+public static void main(String args[])   
+{            
+int x = 33, y = 55, a = -15, b = -23, sum, s;    
+sum=Integer.sum(x, y);  
+//returns the sum of x and y  
+System.out.println("The sum of x and y is: " +sum);    
+s=Integer.sum(a, b);  
+System.out.println("The sum of x and y is: " +s);  
+}    
+}   
