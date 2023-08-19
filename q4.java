1e22cc74a65f4e4b3016187e7f9b2b77764baba0
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-08-19 17:46:04 +0530

    increment & decrement in java

---
+++ q4.java
@@ -0,0 +1,18 @@
+// increment & decrement operator 
+public class SumOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+    int i = 56;
+    System.out.println(i++);
+    System.out.println(i);
+    System.out.println(++i);
+    System.out.println(i);
+
+    //decrement 
+    System.out.println(i--);
+    System.out.println(i);
+    System.out.println(--i);
+    System.out.println(i);
+}  
+}  
